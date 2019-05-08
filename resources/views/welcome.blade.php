da4ef2b6344d136b06ba94e7f869dc363779db49
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-05-08 13:39:52 -0500

    more front page styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,10 @@
   <link href='https://fonts.googleapis.com/css?family=Lato:400,700,300|Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>
 
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" href="/css/main.css">
-
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ"
+  crossorigin="anonymous">
+  <link href="/css/main.css" rel="stylesheet" type="text/css">
   <link rel="icon" href="./favicon.ico"> <!-- TAB ICON -->
 
   
@@ -20,27 +22,51 @@
 </head>
 <body>
   <!--header-->
-  <header class="bg-dark text-light">
-    <nav class="container d-flex justify-content-between">
-      <div class="menu-site-name"><h3>TW</h3></div>
-      
-      <ul class="nav-links d-flex">
-        <li class="nav-link">
-          <a href="#">Projects</a>
-        </li>
-        <li class="nav-link">
-          <a href="#">About Me</a>
-        </li>
-        <li class="nav-link">
-          <a href="#">Blog</a>
-        </li>
-        <li class="nav-link">
-          <a href="#">Contact</a>
-        </li>
-      </ul>
+  <header class="text-light">
+    <nav class="shadow py-2">
+      <div class="container d-flex justify-content-between">
+        <div class="menu-site-name"><h3>TW</h3></div>
+        <ul class="nav-links d-flex">
+          <li>
+            <a href="#" class="nav-link">Projects</a>
+          </li>
+          <li>
+            <a href="#" class="nav-link">About Me</a>
+          </li>
+          <li>
+            <a href="#" class="nav-link">Blog</a>
+          </li>
+          <li>
+            <a href="#" class="nav-link">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
-    <!--/ header-->
+  <!--/ header-->
+  <section class="jumbotron text-center">
+    <div class="container">
+      <h1 class="jumbotron-heading">Taylor Wilkinson</h1>
+      <p class="lead text-muted">Michigander and passionate web developer looking for opportunities to gain experience and collaborate with awesome people.</p>
+      <p>
+        <a href="/contact" class="btn btn-primary my-2">Get In Touch</a>
+      </p>
+      <ul class="jumbotron-social-icons d-flex justify-content-around mx-auto">
+          <li>
+               <a href="https://github.com/coding4tacos/" class="text-custom-dark" target="_blank"><i class="fab fa-github"></i> </a>
+          </li>
+          <li>
+               <a href="https://twitter.com/coding4tacos/" class="text-custom-dark" target="_blank"> <i class="fab fa-twitter"></i> </a>
+          </li>
+          <li>
+               <a href="https://codepen.io/coding4tacos/" class="text-custom-dark" target="_blank"> <i class="fab fa-codepen"></i> </a>
+          </li>
+          <li>
+               <a href="https://www.linkedin.com/in/taylor-wilkinson-a6478229/"  class="text-custom-dark" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+          </li>
+     </ul>
+    </div>
+  </section>
   
 
 
