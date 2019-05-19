b28102cf37bddd5b3eacd59d31b1f5d529bf11e5
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-05-19 17:33:24 -0500

    finished dashboard styles until next lesson

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,55 +1,53 @@
 @extends('layouts.admin')
 @section('content')
  
-       <section class="users">
-           <div class="container">
-               <div class="row">
-                   <div class="card card-default border-0 bg-light mx-auto">
-                      <div class="card-heading">
-                           <h3 class="text-center">Users</h3>
-                           <div class="text-right">
-                              <a href="{{ route('users.create') }}" class="btn btn-complete text-primary"><i class="fa fa-plus"></i> New User</a>
-                           </div>
-                      </div>
-                           <div class="card-body">
-                                <div id="tableWithDynamicRows_wrapper" class="dataTables_wrapper form-inline no-footer">
-                                     <div class="table-responsive overflowFixed">
-                                          <table id="tableWithDynamicRows" class="table table-hover demo-table-dynamic dataTable no-footer" role="grid" aria-describedby="tableWithDynamicRows_info">
-                                               <thead>
-                                                    <tr role="row">
-                                                         <th class="col-md-3">Name</th>
-                                                         <th class="col-md-4">Email</th>
-                                                         <th class="col-md-2">Action</th>
-                                                    </tr>
-                                               </thead>
-                                               <tbody>
-                                               @foreach($users as $user)
-                                               <tr role="row" class="odd">
-                                                    <td class="v-align-middle">
-                                                         <p>
-                                                              {{ $user->name }}
-                                                         </p>
-                                                    </td>
-                                                    <td class="v-align-middle">
-                                                         <p>
-                                                              {{ $user->email }}
-                                                         </p>
-                                                    </td>
-                                                    <td class="v-align-middle">
-                                                        <!-- Botones de accion -->
-                                                    <a href="{{ route('users.show', $user->id) }}">Ver</a>
-                                                    </td>
-                                               </tr>
-                                               @endforeach
-                                               </tbody>
-                                          </table>
-                                          <div class="text-center">{!! $users->render() !!}</div>
-                                     </div>
-                                </div>
-                           </div>
-                      </div>
-                 </div>
-            </div>
+     <section class="users">
+          <div class="container">
+               <div class="card card-default col-sm-6 border-0 bg-light shadow-sm mx-auto">
+                    <div class="card-heading">
+                         <h3 class="text-center">Users</h3>
+                         <div class="text-right">
+                         <a href="{{ route('users.create') }}" class="btn btn-complete text-primary"><i class="fa fa-plus"></i> New User</a>
+                         </div>
+                    </div>
+                         <div class="card-body">
+                              <div id="tableWithDynamicRows_wrapper" class="dataTables_wrapper form-inline no-footer">
+                                   <div class="table-responsive overflowFixed">
+                                        <table id="tableWithDynamicRows" class="table table-hover demo-table-dynamic dataTable no-footer" role="grid" aria-describedby="tableWithDynamicRows_info">
+                                             <thead>
+                                                  <tr role="row">
+                                                       <th class="col-md-3">Name</th>
+                                                       <th class="col-md-4">Email</th>
+                                                       <th class="col-md-2">Action</th>
+                                                  </tr>
+                                             </thead>
+                                             <tbody>
+                                             @foreach($users as $user)
+                                             <tr role="row" class="odd">
+                                                  <td class="v-align-middle">
+                                                       <p>
+                                                            {{ $user->name }}
+                                                       </p>
+                                                  </td>
+                                                  <td class="v-align-middle">
+                                                       <p>
+                                                            {{ $user->email }}
+                                                       </p>
+                                                  </td>
+                                                  <td class="v-align-middle">
+                                                       <!-- Botones de accion -->
+                                                  <a href="{{ route('users.show', $user->id) }}">See</a>
+                                                  </td>
+                                             </tr>
+                                             @endforeach
+                                             </tbody>
+                                        </table>
+                                        <div class="text-center">{!! $users->render() !!}</div>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
        </section>
   
 @endsection
--- resources/views/admin/users/view.blade.php
+++ resources/views/admin/users/view.blade.php
@@ -1,40 +1,34 @@
 @extends('layouts.admin')
 @section('content')
 
-<div class="first-section flex-center margin-top-50">
-	<div class="container" style="margin-top: 180px; margin-bottom: 180px;">
-	    <div class="row">
-	        <div>
-	            <div class="panel panel-default">
-	                <div class="panel-heading panel-heading-right" style="font-size: 14px; font-weight: bold;">Usuario - Ver</div>
-	                <div class="panel-body">
-	                    <div class="col-sm-10 col-sm-offset-1">
-	                        <legend style="margin-bottom: 15px;">Datos del usuario</legend>
-	                        <div class="form-group col-md-6">
-	                            <label class="col-md-10" style="font-weight: bold;">Rol</label>
-	                            <span class="col-md-10">{{ $role->name }}</span>
-	                        </div>
-	                        <div class="form-group col-md-6">
-	                            <label class="col-md-10 margin-top-20" style="font-weight: bold;">Nombre</label>
-	                            <p class="col-md-10">{{ $user->name }}</p>
-	                        </div>
-	                        <div class="form-group col-md-6">
-	                            <label class="col-md-10 margin-top-20" style="font-weight: bold;">Correo</label>
-	                            <p class="col-md-10">{{ $user->email }}</p>
-	                        </div>
-	                    </div>
-	                    <div class="clearfix"></div>
-	                    <br><br><br>
-	                    <div class="form-group col-sm-12 text-center btn-div">
-	                        <div>
-	                            <a href="{{ route('users.main') }}"><button type="button" class="btn btn-primary btn-form">Volver</button></a>
-	                        </div>
-	                    </div>
-	                </div>
-	            </div>
-	        </div>
-	    </div>
+<section class="user-info">
+	<div class="container">
+		<div class="col-sm-6 mx-auto bg-light shadow-sm border-0 card card-default">
+			<div class="card-body">
+					<h3 class="text-center">User Details</h3>
+					<div class="form-group">
+						<label class="col-md-10 mt-3"style="font-weight: bold;">Role</label>
+						<p class="col-md-10">{{ $role->name }}</p>
+					</div>
+					<div class="form-group">
+						<label class="col-md-10 mt-3" style="font-weight: bold;">Name</label>
+						<p class="col-md-10">{{ $user->name }}</p>
+					</div>
+					<div class="form-group">
+						<label class="col-md-10 mt-3" style="font-weight: bold;">email</label>
+						<p class="col-md-10">{{ $user->email }}</p>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+
+				<div class="form-group col-sm-12 text-center btn-div">
+					<div>
+						<a href="{{ route('users.main') }}"><button type="button" class="btn btn-primary btn-form">Back</button></a>
+					</div>
+				</div>
+			</div>
 	</div>
-</div>
+</section>
+
 
 @endsection
