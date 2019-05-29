660e79a2c033f2def26c944296eecde6a92bceb0
Taylor Wilkinson <taylorwlearns@gmail.com>
2019-05-29 15:15:44 -0500

    Revert "working on blog page styles"
    This reverts commit f15802abea371bfcadd467d3cab5afcee2c5320d.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
   <!--/ header-->
   <section class="jumbotron rounded-0 mb-0 text-center">
     <div class="container">
-      <h1 class="jumbotron-heading text-custom-light" id="welcome-name">Taylor Wilkinson</h1>
+      <h1 class="jumbotron-heading text-custom-dark" id="welcome-name">Taylor Wilkinson</h1>
       <p class="lead">Passionate web developer looking for opportunities to gain experience and collaborate with awesome people.</p>
       <p class="jumbo-btns">
         <a href="#" class="btn btn-custom shadow-sm text-custom-dark border my-2">My Work</a>
